30c6264001d419a83d7acf06d06cde4274896fcf
Caleb Gibbs <calebgibbs@me.com>
2021-05-23 18:09:36 +1200

    about and why us cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,50 @@
 <template>
   <div>
     <HeroImage/>  
-    <Services/>
+    <Services/> 
+    <div class="cards">
+      <About/>
+      <Why/>
+    </div>
   </div>
 </template>
 
 <script>
   import HeroImage from '../components/HeroImage' 
-  import Services from '../components/HomeServices'
+  import Services from '../components/HomeServices' 
+  import About from '../components/AboutUs'
+  import Why from '../components/WhyUs'
   export default {
     components: {
       HeroImage, 
-      Services
+      Services, 
+      About, 
+      Why
     }
   }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" scoped> 
+@import '../assets/styles/mediaqueries';
+.cards { 
+  display: grid; 
+  grid-template-columns: repeat(2, 1fr); 
+  margin-top: 6em;
+  margin-bottom: 6em;
+} 
+
+@include mobile-landscape {
+  .cards { 
+    grid-template-columns: repeat(1, 1fr);
+    grid-gap: 2em;
+  }
+} 
+
+@include mobile-portait {
+  .cards { 
+    grid-template-columns: repeat(1, 1fr); 
+    grid-gap: 2em;
+  }
+}
 
 </style>
