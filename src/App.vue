59356118fbe71d2d8740a0a2e8d9adbac13344b3
Caleb Gibbs <calebgibbs@me.com>
2021-05-23 14:39:59 +1200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app"> 
+    <Navigation/>
+    <router-view/>  
+    Hello
+  </div>
+</template>
+
+<script> 
+import Navigation from './components/Navigation'
+  export default {
+    components: {
+      Navigation
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
